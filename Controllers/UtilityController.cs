6fa5aa3ae495679c66ba299b2cc1e2f26dfba576
Jamal <jamal@Jamals-MacBook-Air.local>
2017-05-19 01:11:38 +0430

    some utilities for mockdata

---
+++ Controllers/UtilityController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication
+{
+
+    //[Route("api/[controller]")]
+    public class UtitlityController : Controller
+    {
+
+        [Route("api/ShowCases")]
+        public JsonResult GetTrends(string companyName)
+        {
+            var trends = MockData.Trends;
+            return Json(trends);
+        }
+
+        [Route("api/bestdeals")]
+        public JsonResult BestDeals()
+        {
+            var trends = MockData.CompniesProducts;
+            return Json(trends);
+        }
+
+         [Route("api/todayDeals")]
+        public JsonResult TodayDeals()
+        {
+            var trends = MockData.CompniesProducts;
+            return Json(trends);
+        }
+
+
+         [Route("api/search")]
+        public JsonResult findProducts()
+        {
+            var trends = MockData.CompniesProducts;
+            return Json(trends);
+        }
+
+        [Route("api/trendProducts")]
+          public JsonResult getTrendProdcts()
+        {
+            var products = MockData.CompniesProducts;
+            return Json(products);
+        }
+
+    }
+}
